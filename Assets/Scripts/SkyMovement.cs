2f48574e41ee16d437234c4e608e322624219136
Alexander Boyarintsev <alex.boyarintsev@icloud.com>
2021-12-15 01:34:26 +0300

    # SkyMovement added

---
+++ Assets/Scripts/SkyMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Vector3 _startPosition;
+    private float _offset = -5f;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.left * _speed, Space.World);
+        if (transform.position.x < _offset)
+        {
+            transform.position = _startPosition;
+        }
+    }
+}
