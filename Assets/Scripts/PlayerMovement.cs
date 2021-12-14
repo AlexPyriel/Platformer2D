f58bb7364205c29afe5639905470578dc2fabb31
Alexander Boyarintsev <alex.boyarintsev@icloud.com>
2021-12-14 22:26:37 +0300

    # Player animations added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float _speed = 1.5f;
-    [SerializeField] private float _jumpForce = 3f;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
 
     private Rigidbody2D _rigidBody;
+    private Animator _animator;
     private float _velocity;
+    private bool _grounded;
 
     private void Awake()
     {
         _rigidBody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -30,15 +34,28 @@ public class PlayerMovement : MonoBehaviour
 
         if (Mathf.Approximately(0, _velocity) == false)
         {
+            _animator.SetBool("isWalking", true);
             transform.rotation = _velocity < 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+        }
+        else
+        {
+            _animator.SetBool("isWalking", false);
         }
     }
 
     private void Jump()
     {
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidBody.velocity.y) < 0.001f)
+        if (Input.GetButtonDown("Jump") && _grounded)
         {
             _rigidBody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            _grounded = false;
+            _animator.SetBool("isJumping", true);
+        }
+
+        if (_rigidBody.velocity.y == 0)
+        {
+            _grounded = true;
+            _animator.SetBool("isJumping", false);
         }
     }
 }
